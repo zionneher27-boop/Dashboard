d8be24fb48f896fb840277f95ceec9af3c714281
zionneher27-boop <zionneher27@gmx.de>
2026-06-08 23:51:01 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'life-dashboard-v1';
+const ASSETS = ['./index.html','./manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(()=>caches.match('./index.html'))));
+});
